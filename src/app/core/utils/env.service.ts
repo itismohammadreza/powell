aca9aa97cec35043a20fd53d9949cf9e5eb8f7ae
Mohammad Reza <shahbaziweb@gmail.com>
2022-08-25 13:30:43 +0430

    =

--- src/app/core/utils/env.service.ts
+++ src/app/core/utils/env.service.ts
@@ -5,9 +5,6 @@ import {Injectable} from '@angular/core';
 })
 export class EnvService {
   // The values that are defined here are the default values that can be overridden by env.js
-  apiUrl = 'https://vcdemoapi.iranlms.ir/api/v1';
-  socketUrl: 'wss://vcdemoapi.iranlms.ir/ws/v1';
-
-  constructor() {
-  }
+  apiUrl = '';
+  socketUrl = '';
 }
