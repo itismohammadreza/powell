6bf95e48bbddf74c15c0c2a4ea0564a9c4317457
Mohammad Reza <shahbaziweb@gmail.com>
2022-08-29 23:58:07 +0430

    create gregorian datepicker

---
+++ src/app/modules/main/pages/showcase/pages/gregorian-datepicker/gregorian-datepicker.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-gregorian-datepicker-page',
+  templateUrl: './gregorian-datepicker.page.html',
+  styleUrls: ['./gregorian-datepicker.page.scss']
+})
+export class GregorianDatepickerPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/ng/components/gregorian-datepicker/gregorian-datepicker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-gregorian-datepicker',
+  templateUrl: './gregorian-datepicker.component.html',
+  styleUrls: ['./gregorian-datepicker.component.scss']
+})
+export class GregorianDatepickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
