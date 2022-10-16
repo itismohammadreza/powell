029d0f9f61f312e47d72b30a1daab75e66879346
PC-22 <shahbaziweb@gmail.com>
2022-10-16 15:21:58 +0330

    .

--- src/app/ng/components/infinite-scroll/infinite-scroll.component.ts
+++ src/app/ng/components/infinite-scroll/infinite-scroll.component.ts
@@ -1,33 +1,54 @@
-import {Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'ng-infinite-scroll',
   templateUrl: './infinite-scroll.component.html',
   styleUrls: ['./infinite-scroll.component.scss']
 })
-export class InfiniteScrollComponent implements OnInit, OnDestroy {
+export class InfiniteScrollComponent implements AfterContentInit, OnDestroy {
 
+  @Input() spinnerWidth: string = '40px';
   @Output() scrolled = new EventEmitter();
   @ViewChild('anchor', {static: true}) anchor: ElementRef<HTMLElement>;
 
   observer: IntersectionObserver;
+  loading: boolean;
 
   constructor(private el: ElementRef) {
   }
 
-  ngOnInit() {
+  ngAfterContentInit() {
     this.observer = new IntersectionObserver(([entry]) => {
-      entry.isIntersecting && this.scrolled.emit();
+      if (entry.isIntersecting) {
+        this.showLoading()
+        this.scrolled.emit(this.hideLoading);
+      }
     }, {
       root: this.isHostScrollable() ? this.el.nativeElement : null
     });
-
     this.observer.observe(this.anchor.nativeElement);
   }
 
-  private isHostScrollable() {
-    const style = window.getComputedStyle(this.el.nativeElement);
+  showLoading = () => {
+    this.loading = true;
+  }
 
+  hideLoading = () => {
+    this.loading = false
+  }
+
+  isHostScrollable() {
+    const style = window.getComputedStyle(this.el.nativeElement);
     return style.getPropertyValue('overflow') === 'auto' ||
       style.getPropertyValue('overflow-y') === 'scroll';
   }
