8a0dff0d01239c5348aaa2623038b0634b02a57b
PC-22 <shahbaziweb@gmail.com>
2023-02-15 11:24:42 +0330

    create config.ts file

---
+++ src/app/ng/models/config.ts
@@ -0,0 +1,56 @@
+import {NgFixLabelPosition, NgLabelPosition} from "@ng/models/forms";
+
+export interface NgConfig {
+  ripple?: boolean;
+  rtl?: boolean;
+  theme?: NgTheme;
+  defaultLabelPos?: NgLabelPosition,
+  defaultFixLabelPos?: NgFixLabelPosition,
+}
+
+export type NgTheme =
+  'arya-blue' |
+  'arya-green' |
+  'arya-orange' |
+  'arya-purple' |
+  'bootstrap4-dark-blue' |
+  'bootstrap4-dark-purple' |
+  'bootstrap4-light-blue' |
+  'bootstrap4-light-purple' |
+  'fluent-light' |
+  'lara-dark-indigo' |
+  'lara-dark-purple' |
+  'lara-light-indigo' |
+  'lara-light-purple' |
+  'luna-amber' |
+  'luna-blue' |
+  'luna-green' |
+  'luna-pink' |
+  'md-dark-deeppurple' |
+  'md-dark-indigo' |
+  'md-light-deeppurple' |
+  'md-light-indigo' |
+  'mdc-dark-deeppurple' |
+  'mdc-dark-indigo' |
+  'mdc-light-deeppurple' |
+  'mdc-light-indigo' |
+  'mira' |
+  'nano' |
+  'nova' |
+  'nova-accent' |
+  'nova-alt' |
+  'rhea' |
+  'saga-blue' |
+  'saga-green' |
+  'saga-orange' |
+  'saga-purple' |
+  'soho-dark' |
+  'soho-light' |
+  'tailwind-light' |
+  'vela-blue' |
+  'vela-green' |
+  'vela-orange' |
+  'vela-purple' |
+  'viva-dark' |
+  'viva-light';
+
