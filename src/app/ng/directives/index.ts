3a0b771e6a27c14004bcef70d29460163e2a58f3
PC-22 <shahbaziweb@gmail.com>
2023-02-21 16:25:46 +0330

    remove ripple directive

--- src/app/ng/directives/index.ts
+++ src/app/ng/directives/index.ts
@@ -2,13 +2,11 @@ import {Type} from '@angular/core';
 import {AddonDirective} from './addon.directive';
 import {ClickOutsideDirective} from './click-outside.directive';
 import {NumberOnlyDirective} from './number-only.directive';
-import {RippleDirective} from './ripple.directive';
 import {TemplateDirective} from '@ng/directives/template.directive';
 
 export const DIRECTIVES: Type<any>[] = [
   AddonDirective,
   NumberOnlyDirective,
   ClickOutsideDirective,
-  RippleDirective,
   TemplateDirective,
 ];
