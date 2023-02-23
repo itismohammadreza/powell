0d47ac10e22a43e6cb977b2dd07af6222c7f9f63
Mohammad Reza <shahbaziweb@gmail.com>
2023-02-23 15:20:01 +0330

    add onDestroy in config-handler.directive.ts

--- src/app/ng/directives/config-handler.directive.ts
+++ src/app/ng/directives/config-handler.directive.ts
@@ -1,20 +1,28 @@
-import {Directive, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Directive, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {ConfigService} from "@ng/services";
+import {Subject, takeUntil} from "rxjs";
 
 @Directive({
   selector: '[ngConfigHandler]'
 })
-export class ConfigHandlerDirective implements OnInit {
+export class ConfigHandlerDirective implements OnInit, OnDestroy {
   @Input() rtl: boolean;
   @Output() rtlChange = new EventEmitter();
+
+  destroy$: Subject<any> = new Subject<any>()
 
   constructor(private configService: ConfigService) {
   }
 
   ngOnInit() {
-    this.configService.configChange$.subscribe(res => {
+    this.configService.configChange$.pipe(takeUntil(this.destroy$)).subscribe(res => {
       this.rtl = res.rtl;
       this.rtlChange.emit(this.rtl);
     })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
