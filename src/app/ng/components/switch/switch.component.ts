b9615a9381e834080e7d3e0f2c285f9a2d9ea2ef
PC-22 <shahbaziweb@gmail.com>
2023-02-27 18:31:45 +0330

    implement usage of configService in switch

--- src/app/ng/components/switch/switch.component.ts
+++ src/app/ng/components/switch/switch.component.ts
@@ -10,7 +10,7 @@ import {
   NgControl,
 } from '@angular/forms';
 import {NgFixLabelPosition, NgValidation} from '@ng/models/forms';
-import {NgGlobal} from "@ng/ng-global";
+import {ConfigService} from "@ng/services";
 
 @Component({
   selector: 'ng-switch',
@@ -29,9 +29,9 @@ export class SwitchComponent implements OnInit, ControlValueAccessor {
   @Input() label: string;
   @Input() labelWidth: number;
   @Input() hint: string;
-  @Input() rtl: boolean = NgGlobal.config.rtl;
-  @Input() showRequiredStar: boolean = NgGlobal.config.showRequiredStar;
-  @Input() labelPos: NgFixLabelPosition = NgGlobal.config.fixLabelPos;
+  @Input() rtl: boolean = this.configService.getConfig().rtl;
+  @Input() showRequiredStar: boolean = this.configService.getConfig().showRequiredStar;
+  @Input() labelPos: NgFixLabelPosition = this.configService.getConfig().fixLabelPos;
   @Input() validation: NgValidation;
   @Input() onLabel: string;
   @Input() offLabel: string;
@@ -57,7 +57,7 @@ export class SwitchComponent implements OnInit, ControlValueAccessor {
   onModelTouched: any = () => {
   };
 
-  constructor(private cd: ChangeDetectorRef, private injector: Injector) {
+  constructor(private cd: ChangeDetectorRef, private injector: Injector, private configService: ConfigService) {
   }
 
   ngOnInit() {
