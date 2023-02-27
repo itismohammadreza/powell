21b80eff14456bf034a8ce9cdba26a1a54f04b47
PC-22 <shahbaziweb@gmail.com>
2023-02-27 18:31:11 +0330

    implement usage of configService in rating

--- src/app/modules/main/pages/showcase/pages/rating/rating.page.ts
+++ src/app/modules/main/pages/showcase/pages/rating/rating.page.ts
@@ -1,7 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {NgFixLabelPosition} from '@ng/models/forms';
-import {NgGlobal} from "@ng/ng-global";
+import {ConfigService} from "@ng/services";
 
 @Component({
   selector: 'ng-rating-page',
@@ -17,8 +17,8 @@ export class RatingPage {
   label: string = 'label';
   labelWidth: number = 100;
   hint: string = '';
-  rtl: boolean = NgGlobal.config.rtl;
-  labelPos: NgFixLabelPosition = NgGlobal.config.fixLabelPos;
+  rtl: boolean = this.configService.getConfig().rtl;
+  labelPos: NgFixLabelPosition = this.configService.getConfig().fixLabelPos;
   // native properties
   stars: number = 5;
   cancel: boolean = true;
@@ -27,4 +27,7 @@ export class RatingPage {
   iconOnClass: string = 'pi pi-star-fill';
   iconOffClass: string = 'pi pi-star';
   iconCancelClass: string = 'pi pi-ban';
+
+  constructor(private configService: ConfigService) {
+  }
 }
--- src/app/ng/components/rating/rating.component.ts
+++ src/app/ng/components/rating/rating.component.ts
@@ -20,7 +20,7 @@ import {
   NgControl,
 } from '@angular/forms';
 import {NgFixLabelPosition, NgValidation} from '@ng/models/forms';
-import {NgGlobal} from "@ng/ng-global";
+import {ConfigService} from "@ng/services";
 
 @Component({
   selector: 'ng-rating',
@@ -39,9 +39,9 @@ export class RatingComponent implements OnInit, AfterViewInit, ControlValueAcces
   @Input() label: string;
   @Input() labelWidth: number;
   @Input() hint: string;
-  @Input() rtl: boolean = NgGlobal.config.rtl;
-  @Input() showRequiredStar: boolean = NgGlobal.config.showRequiredStar;
-  @Input() labelPos: NgFixLabelPosition = NgGlobal.config.fixLabelPos;
+  @Input() rtl: boolean = this.configService.getConfig().rtl;
+  @Input() showRequiredStar: boolean = this.configService.getConfig().showRequiredStar;
+  @Input() labelPos: NgFixLabelPosition = this.configService.getConfig().fixLabelPos;
   @Input() validation: NgValidation;
   @Input() disableConfigChangeEffect: boolean;
   // native properties
@@ -66,7 +66,7 @@ export class RatingComponent implements OnInit, AfterViewInit, ControlValueAcces
   onModelTouched: any = () => {
   };
 
-  constructor(private cd: ChangeDetectorRef, private injector: Injector) {
+  constructor(private cd: ChangeDetectorRef, private injector: Injector, private configService: ConfigService) {
   }
 
   ngOnInit() {
