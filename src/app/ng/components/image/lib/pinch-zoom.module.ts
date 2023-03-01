0d6afc46eeceefbb0dac54533bd9197bbc88f776
PC-22 <shahbaziweb@gmail.com>
2023-03-01 11:53:28 +0330

    create pinch-zoom.component

---
+++ src/app/ng/components/image/lib/pinch-zoom.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PinchZoomComponent } from './pinch-zoom.component';
+
+@NgModule({
+    declarations: [
+        PinchZoomComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        PinchZoomComponent
+    ],
+    providers: [
+
+    ],
+    bootstrap: [
+        
+    ],
+    entryComponents: [
+        PinchZoomComponent
+    ]
+})
+export class PinchZoomModule { }
