8847e0d60d08f8206e6b604de385326e7b146181
Mohammad Reza <shahbaziweb@gmail.com>
2023-03-03 17:27:31 +0330

    create image.component

--- src/app/ng/components/image/image.component.ts
+++ src/app/ng/components/image/image.component.ts
@@ -1,18 +1,49 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ContentChildren,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+  TemplateRef,
+  ViewChild
+} from '@angular/core';
+import {animate, AnimationEvent, style, transition, trigger} from "@angular/animations";
+import {ConfigService} from "@ng/services";
+import {TemplateDirective} from "@ng/directives/template.directive";
+import {SafeUrl} from "@angular/platform-browser";
+import {ZIndexUtils} from "primeng/utils";
+import {DomHandler} from "primeng/dom";
 
 @Component({
   selector: 'ng-image',
   templateUrl: './image.component.html',
-  styleUrls: ['./image.component.scss']
+  styleUrls: ['./image.component.scss'],
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({transform: 'scale(0.7)', opacity: 0}), animate('{{showTransitionParams}}')
+      ]),
+      transition('visible => void', [
+        animate('{{hideTransitionParams}}', style({transform: 'scale(0.7)', opacity: 0}))
+      ])
+    ])
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'p-element'
+  }
 })
-export class ImageComponent implements OnInit {
-  @Input() onErrorImagePlaceholder: string;
-  // native properties
+export class ImageComponent implements AfterContentInit {
   @Input() imageClass: string;
   @Input() imageStyle: any;
   @Input() styleClass: string;
   @Input() style: any;
-  @Input() src: string;
+  @Input() src: string | SafeUrl;
   @Input() alt: string;
   @Input() width: string;
   @Input() height: string;
@@ -20,22 +51,169 @@ export class ImageComponent implements OnInit {
   @Input() preview: boolean = false;
   @Input() showTransitionOptions: string = '150ms cubic-bezier(0, 0, 0.2, 1)';
   @Input() hideTransitionOptions: string = '150ms cubic-bezier(0, 0, 0.2, 1)';
-  @Output() onShow = new EventEmitter();
-  @Output() onHide = new EventEmitter();
-  @Output() onError = new EventEmitter();
+  @Output() onShow: EventEmitter<any> = new EventEmitter();
+  @Output() onHide: EventEmitter<any> = new EventEmitter();
+  @Output() onImageError: EventEmitter<any> = new EventEmitter();
+  @ViewChild('mask') mask: ElementRef;
+  @ContentChildren(TemplateDirective) templates: QueryList<any>;
 
-  constructor(private el: ElementRef) {
+  indicatorTemplate: TemplateRef<any>;
+  maskVisible: boolean = false;
+  previewVisible: boolean = false;
+  rotate: number = 0;
+  scale: number = 1;
+  previewClick: boolean = false;
+  container: HTMLElement;
+  wrapper: HTMLElement;
+  zoomSettings = {
+    default: 1,
+    step: 0.1,
+    max: 1.5,
+    min: 0.5
+  };
+
+  constructor(private config: ConfigService, private cd: ChangeDetectorRef) {
   }
 
-  ngOnInit(): void {
-    this.el.nativeElement.querySelector('img').onerror = (event) => {
-      event.target.onerror = null;
-      event.target.src = "assets/images/no-image-placeholder.jpg" || this.onErrorImagePlaceholder;
-      this.onError.emit(event);
+  ngAfterContentInit() {
+    this.templates.forEach((item) => {
+      switch (item.getType()) {
+        case 'indicator':
+          this.indicatorTemplate = item.template;
+          break;
+
+        default:
+          this.indicatorTemplate = item.template;
+          break;
+      }
+    });
+  }
+
+  onImageClick() {
+    if (this.preview) {
+      this.maskVisible = true;
+      this.previewVisible = true;
     }
   }
 
-  emitter(name: string, event: any) {
-    (this[name] as EventEmitter<any>).emit(event);
+  onMaskClick() {
+    if (!this.previewClick) {
+      this.closePreview();
+    }
+    this.previewClick = false;
   }
+
+  onPreviewImageClick() {
+    this.previewClick = true;
+  }
+
+  rotateRight() {
+    this.rotate += 90;
+    this.previewClick = true;
+  }
+
+  rotateLeft() {
+    this.rotate -= 90;
+    this.previewClick = true;
+  }
+
+  zoomIn() {
+    this.scale = this.scale + this.zoomSettings.step;
+    this.previewClick = true;
+  }
+
+  zoomOut() {
+    this.scale = this.scale - this.zoomSettings.step;
+    this.previewClick = true;
+  }
+
+  onAnimationStart(event: AnimationEvent) {
+    switch (event.toState) {
+      case 'visible':
+        this.container = event.element;
+        this.wrapper = this.container.parentElement;
+        this.appendContainer();
+        this.moveOnTop();
+        break;
+
+      case 'void':
+        DomHandler.addClass(this.wrapper, 'p-component-overlay-leave');
+        break;
+    }
+  }
+
+  onAnimationEnd(event: AnimationEvent) {
+    switch (event.toState) {
+      case 'void':
+        ZIndexUtils.clear(this.wrapper);
+        this.maskVisible = false;
+        this.container = null;
+        this.wrapper = null;
+        this.cd.markForCheck();
+        this.onHide.emit({});
+        break;
+      case 'visible':
+        this.onShow.emit({});
+        break;
+    }
+  }
+
+  moveOnTop() {
+    ZIndexUtils.set('modal', this.wrapper, this.config.getConfig().zIndex.modal);
+  }
+
+  appendContainer() {
+    if (this.appendTo) {
+      if (this.appendTo === 'body') document.body.appendChild(this.wrapper);
+      else DomHandler.appendChild(this.wrapper, this.appendTo);
+    }
+  }
+
+  imagePreviewStyle() {
+    return {transform: 'rotate(' + this.rotate + 'deg) scale(' + this.scale + ')'};
+  }
+
+  containerClass() {
+    return {
+      'p-image p-component': true,
+      'p-image-preview-container': this.preview
+    };
+  }
+
+  handleToolbarClick(event: MouseEvent): void {
+    event.stopPropagation();
+  }
+
+  closePreview() {
+    this.previewVisible = false;
+    this.rotate = 0;
+    this.scale = this.zoomSettings.default;
+  }
+
+  imageError(event) {
+    this.onImageError.emit(event);
+  }
+
+  get isZoomOutDisabled() {
+    return this.scale - this.zoomSettings.step <= this.zoomSettings.min;
+  }
+
+  get isZoomInDisabled() {
+    return this.scale + this.zoomSettings.step >= this.zoomSettings.max;
+  }
+
+  // @Input() onErrorImagePlaceholder: string;
+  // @Output() onError = new EventEmitter();
+  //
+  // ngOnInit(): void {
+  //   this.el.nativeElement.querySelector('img').onerror = (event) => {
+  //     event.target.onerror = null;
+  //     event.target.src = "assets/images/no-image-placeholder.jpg" || this.onErrorImagePlaceholder;
+  //     this.onError.emit(event);
+  //   }
+  // }
+  //
+  // emitter(name: string, event: any) {
+  //   (this[name] as EventEmitter<any>).emit(event);
+  // }
 }
