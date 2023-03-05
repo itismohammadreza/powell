ec52c75742fee20c0c1c59199bbbb2587e2fa1b3
PC-22 <shahbaziweb@gmail.com>
2023-03-05 17:12:49 +0330

    minor updates

--- src/app/core/guard/open-dialog.guard.ts
+++ src/app/core/guard/open-dialog.guard.ts
@@ -1,29 +1,19 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanDeactivate, Router, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot} from '@angular/router';
 import {OverlayService} from "@ng/services";
-import {Location} from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenDialogGuard implements CanDeactivate<any> {
-  constructor(private overlayService: OverlayService,
-              private router: Router,
-              private location: Location) {
+  constructor(private overlayService: OverlayService) {
   }
 
   async canDeactivate(component: any,
                       currentRoute: ActivatedRouteSnapshot,
                       currentState: RouterStateSnapshot,
                       nextState?: RouterStateSnapshot): Promise<boolean> {
-    // if (this.overlayService.isAnyDialogOpen()) {
     await this.overlayService.closeAnyOpenDialog();
-    // const currentUrlTree = this.router.createUrlTree([], currentRoute);
-    // const currentUrl = currentUrlTree.toString();
-    // this.location.go(currentUrl);
-    // return false;
-    // } else {
     return true;
-    // }
   }
 }
