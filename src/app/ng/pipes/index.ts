4086e5b7e1a70e69e0ec09ceea596a8ba38d96bb
PC-22 <shahbaziweb@gmail.com>
2023-03-06 17:30:03 +0330

    create label-star.pipe

--- src/app/ng/pipes/index.ts
+++ src/app/ng/pipes/index.ts
@@ -5,12 +5,14 @@ import {FileNamePipe} from './file-name.pipe';
 import {FilterPipe} from './filter.pipe';
 import {SafePipe} from './safe.pipe';
 import {ToBase64Pipe} from './to-base64.pipe';
+import {LabelStarPipe} from "@ng/pipes/label-star.pipe";
 
 export const PIPES: Type<any>[] = [
   DotsPipe,
   SafePipe,
   DatePipe,
   FilterPipe,
+  LabelStarPipe,
   FileNamePipe,
   ToBase64Pipe,
 ];
