d3697337280645e33449ecb9267ce065a5794bbc
PC-22 <shahbaziweb@gmail.com>
2023-03-13 17:12:32 +0330

    create layout.module

---
+++ src/app/modules/layout/index.ts
@@ -0,0 +1,8 @@
+import {Type} from '@angular/core';
+import {LoadingComponent} from "@modules/layout/loading/loading.component";
+import {NotFoundPage} from "@modules/layout/not-found/not-found.page";
+
+export const COMPONENTS: Type<any>[] = [
+  LoadingComponent,
+  NotFoundPage
+];
