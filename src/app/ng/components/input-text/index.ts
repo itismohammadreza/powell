df51c6bdddf8e8cbbe017057f2169ef39b508a73
PC-22 <shahbaziweb@gmail.com>
2023-03-13 17:20:17 +0330

    create input-text.module

---
+++ src/app/ng/components/input-text/index.ts
@@ -0,0 +1,2 @@
+export * from './input-text.component';
+export * from './input-text.module';
---
+++ src/app/ng/components/input-text/input-text.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from "@angular/core";
+import {InputTextModule as PInputTextModule} from 'primeng/inputtext';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {ConfigHandlerModule} from "@ng/directives/config-handler";
+import {AddonModule} from "@ng/directives/addon";
+import {InputTextComponent} from "@ng/components/input-text";
+import {LabelStarModule} from "@ng/pipes/label-star";
+import {KeyFilterModule} from "primeng/keyfilter";
+
+@NgModule({
+  declarations: [InputTextComponent],
+  imports: [PInputTextModule, KeyFilterModule, ConfigHandlerModule, AddonModule, LabelStarModule, CommonModule, FormsModule],
+  exports: [InputTextComponent]
+})
+export class InputTextModule {
+}
