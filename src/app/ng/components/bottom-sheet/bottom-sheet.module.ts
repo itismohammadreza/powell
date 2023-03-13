4be5a7c5f53e1cae36026bf3a47671acbf159084
PC-22 <shahbaziweb@gmail.com>
2023-03-13 18:29:57 +0330

    add missed dependencies to bottom-sheet.module

--- src/app/ng/components/bottom-sheet/bottom-sheet.module.ts
+++ src/app/ng/components/bottom-sheet/bottom-sheet.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {SidebarModule} from "primeng/sidebar";
 import {BottomSheetComponent} from "@ng/components/bottom-sheet";
+import {ButtonModule} from "@ng/components/button";
 
 @NgModule({
   declarations: [BottomSheetComponent],
-  imports: [SidebarModule, CommonModule],
+  imports: [SidebarModule, ButtonModule, CommonModule],
   exports: [BottomSheetComponent]
 })
 export class BottomSheetModule {
