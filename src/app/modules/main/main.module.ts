eaf9fea57eab64bf8775b2b8fa858d9f9da04aff
PC-22 <shahbaziweb@gmail.com>
2023-03-13 18:30:13 +0330

    add missed dependencies to main.module

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
-import {SharedModule} from '@shared/shared.module';
+// import {SharedModule} from '@shared/shared.module';
 import {MainRoutingModule} from './main-routing.module';
 import {COMPONENTS} from ".";
+import {ScrollTopModule} from "primeng/scrolltop";
+import {LayoutModule} from "@modules/layout/layout.module";
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [MainRoutingModule, SharedModule],
+  imports: [MainRoutingModule, ScrollTopModule, LayoutModule],
 })
 export class MainModule {
 }
