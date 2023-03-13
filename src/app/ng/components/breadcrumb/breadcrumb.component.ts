a2c37e05323d16ecc5b12d15f986a9bf72d3141f
PC-22 <shahbaziweb@gmail.com>
2023-03-13 14:52:45 +0330

    implement breadcrumb.service into breadcrumb.component

--- src/app/ng/components/breadcrumb/breadcrumb.component.ts
+++ src/app/ng/components/breadcrumb/breadcrumb.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
-import {Observable} from 'rxjs';
-import {BreadcrumbService} from '@ng/services';
+import {ActivatedRouteSnapshot, Data, NavigationEnd, Router} from "@angular/router";
+import {BehaviorSubject, Observable} from 'rxjs';
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'ng-breadcrumb',
@@ -9,8 +10,35 @@ import {BreadcrumbService} from '@ng/services';
 })
 export class BreadcrumbComponent {
   breadcrumbs$: Observable<any[]>;
+  breadcrumbSubject = new BehaviorSubject<any[]>([]);
 
-  constructor(private breadcrumbService: BreadcrumbService) {
-    this.breadcrumbs$ = breadcrumbService.breadcrumbs$;
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe(event => {
+      const root = this.router.routerState.snapshot.root;
+      const breadcrumbs: any[] = [];
+      this.addBreadcrumb(root, [], breadcrumbs);
+      this.breadcrumbSubject.next(breadcrumbs);
+      this.breadcrumbs$ = this.breadcrumbSubject.asObservable();
+    });
+  }
+
+  addBreadcrumb(route: ActivatedRouteSnapshot, parentUrl: string[], breadcrumbs: any[]) {
+    if (route) {
+      const routeUrl = parentUrl.concat(route.url.map(url => url.path));
+      if (route.data.breadcrumb) {
+        const breadcrumb = {
+          label: this.getLabel(route.data),
+          routerLink: '/' + routeUrl.join('/')
+        };
+        breadcrumbs.push(breadcrumb);
+      }
+      this.addBreadcrumb(route.firstChild, routeUrl, breadcrumbs);
+    }
+  }
+
+  getLabel(data: Data) {
+    return typeof data.breadcrumb === 'function' ? data.breadcrumb(data) : data.breadcrumb;
   }
 }
