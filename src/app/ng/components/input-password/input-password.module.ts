02fe8012f8080d6541c7673ceef3eaeafb117478
PC-22 <shahbaziweb@gmail.com>
2023-03-13 17:20:03 +0330

    create input-password.module

---
+++ src/app/ng/components/input-password/input-password.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+import {PasswordModule} from 'primeng/password';
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {ConfigHandlerModule} from "@ng/directives/config-handler";
+import {AddonModule} from "@ng/directives/addon";
+import {LabelStarModule} from "@ng/pipes/label-star";
+import {InputPasswordComponent} from "@ng/components/input-password";
+
+@NgModule({
+  declarations: [InputPasswordComponent],
+  imports: [PasswordModule, ConfigHandlerModule, AddonModule, LabelStarModule, CommonModule, FormsModule],
+  exports: [InputPasswordComponent]
+})
+export class InputPasswordModule {
+}
