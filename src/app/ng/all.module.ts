b4865a99a7d0fdcaa5f047825b5bbb2ff62b0ebe
PC-22 <shahbaziweb@gmail.com>
2023-03-13 12:16:55 +0330

    minor updates

--- src/app/ng/all.module.ts
+++ src/app/ng/all.module.ts
@@ -1,6 +1,5 @@
-import {Injector, ModuleWithProviders, NgModule} from '@angular/core';
-import {DOCUMENT} from "@angular/common";
-import {ConfirmationService, FilterService, MessageService, PrimeNGConfig} from 'primeng/api';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {ConfirmationService, FilterService, MessageService} from 'primeng/api';
 import {DialogService} from 'primeng/dynamicdialog';
 import {NgComponentsModule} from '@ng/components/components.module';
 
@@ -13,17 +12,6 @@ export class NgAllModule {
     return {
       ngModule: NgAllModule,
       providers: [
-        {
-          provide: ConfigService,
-          useFactory: (primengConfig: PrimeNGConfig, themeService: ThemeService, document: Document) => {
-            const configService = new ConfigService(primengConfig, themeService, document);
-            configService.setConfig(ngConfig);
-            themeService.initTheme();
-            themeService.changeTheme(configService.getConfig().theme);
-            return configService;
-          },
-          deps: [PrimeNGConfig, ThemeService, DOCUMENT]
-        },
         MessageService,
         DialogService,
         ConfirmationService,
