d1bd1e1450634d07bcbae25e2b6e2a609485592e
PC-22 <shahbaziweb@gmail.com>
2023-03-13 17:16:55 +0330

    create dynamic-dialog.module

--- src/app/modules/main/pages/showcase/components/dynamic-dialog-sample/dynamic-dialog-sample.component.ts
+++ src/app/modules/main/pages/showcase/components/dynamic-dialog-sample/dynamic-dialog-sample.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {DynamicDialogConfig} from '@ng/components/dynamic-dialog/dynamic-dialog-config';
-import {DynamicDialogRef} from '@ng/components/dynamic-dialog/dynamic-dialog-ref';
+import {DynamicDialogConfig, DynamicDialogRef} from '@ng/components/dynamic-dialog';
 
 @Component({
   selector: 'ng-dynamic-dialog-sample',
