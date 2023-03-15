16cf7b436292a43225223192c79bf30b0bb7ca04
PC-22 <shahbaziweb@gmail.com>
2023-03-15 14:45:14 +0330

    remove directives.module

--- src/app/ng/directives/directives.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core';
-import {DIRECTIVES} from '.';
-
-@NgModule({
-  declarations: [...DIRECTIVES],
-  exports: [...DIRECTIVES]
-})
-export class NgDirectivesModule {
-}
