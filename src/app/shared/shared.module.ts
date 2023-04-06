b8d6d7fac1cc525839f05c90401e5110243d60b0
Mohammad Reza <shahbaziweb@gmail.com>
2023-04-06 14:11:53 +0430

    minor updates and improvements

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,15 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import {TranslateModule} from '@ngx-translate/core';
 import {COMPONENTS} from '.';
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
-    TranslateModule.forChild(),
-    RouterModule,
-  ],
   exports: [
     ...COMPONENTS,
+  ],
+  imports: [
     CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
-    TranslateModule,
-  ]
+  ],
 })
 export class SharedModule {
 }
