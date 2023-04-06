da9af1313366d0db909b5ebef4d5cace28f440f9
Mohammad Reza <shahbaziweb@gmail.com>
2023-04-06 16:06:25 +0430

    minor updates and improvements

--- src/app/modules/main/index.ts
+++ src/app/modules/main/index.ts
@@ -1,9 +1,16 @@
 import {Type} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {TranslateModule} from "@ngx-translate/core";
 import {MainPage} from "@modules/main/main.page";
 import {NavbarMenuComponent} from "@modules/main/components/navbar-menu/navbar-menu.component";
 import {MainRoutingModule} from "@modules/main/main-routing.module";
 import {LayoutModule} from "@modules/layout/layout.module";
 import {ButtonModule} from "@ng/components/button";
+import {InputTextModule} from "@ng/components/input-text";
+import {DropdownModule} from "@ng/components/dropdown";
+import {SwitchModule} from "@ng/components/switch";
+import {FilterModule} from "@ng/pipes/filter";
 import {
   PrimeAvatarModule,
   PrimeDividerModule,
@@ -13,13 +20,6 @@ import {
   PrimeScrollTopModule,
   PrimeSidebarModule
 } from "@ng/primeng";
-import {InputTextModule} from "@ng/components/input-text";
-import {DropdownModule} from "@ng/components/dropdown";
-import {SwitchModule} from "@ng/components/switch";
-import {FilterModule} from "@ng/pipes/filter";
-import {FormsModule} from "@angular/forms";
-import {TranslateModule} from "@ngx-translate/core";
-import {CommonModule} from "@angular/common";
 
 export const DECLARATIONS: Type<any>[] = [
   MainPage,
