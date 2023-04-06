d6a5371ef79e6dd923ed671927333f88928ffab1
Mohammad Reza <shahbaziweb@gmail.com>
2023-04-07 01:40:01 +0430

    move imported modules from app.module to core.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,19 @@
-import {APP_INITIALIZER, Injector, NgModule} from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
-import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CoreModule} from "@core/core.module";
-import {EnvServiceProvider, TranslationService} from "@core/utils";
-import {initiateNgConfigProvider} from "@ng/api";
-import {NgGlobal} from "@core/config";
-
-export function httpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
-
-export function initiateLanguage(translationService: TranslationService) {
-  return (): Promise<any> => {
-    return translationService.init();
-  }
-}
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
-    CommonModule,
     HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
-  ],
-  providers: [
-    EnvServiceProvider,
-    {provide: APP_INITIALIZER, useFactory: initiateLanguage, deps: [TranslationService], multi: true},
-    initiateNgConfigProvider()
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(private injector: Injector) {
-    NgGlobal.Injector = this.injector;
-  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,26 +1,56 @@
-import {NgModule} from '@angular/core';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {APP_INITIALIZER, Injector, NgModule} from '@angular/core';
+import {HTTP_INTERCEPTORS, HttpClient} from '@angular/common/http';
 import {AuthInterceptor, HttpHandlerInterceptor} from '@core/interceptors';
 import {
   AnimateOnScrollService,
   DynamicDialogService,
+  initiateNgConfigProvider,
   MomentService,
   OverlayService,
   PersianService,
   UtilsService
 } from "@ng/api";
+import {EnvServiceProvider, TranslationService} from "@core/utils";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {NgGlobal} from "@core/config";
+
+export function httpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+export function initiateLanguage(translationService: TranslationService) {
+  return (): Promise<any> => {
+    return translationService.init();
+  }
+}
 
 @NgModule({
+  imports: [
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: HttpHandlerInterceptor, multi: true},
+    {provide: APP_INITIALIZER, useFactory: initiateLanguage, deps: [TranslationService], multi: true},
+    initiateNgConfigProvider(),
+    EnvServiceProvider,
     AnimateOnScrollService,
     DynamicDialogService,
     MomentService,
     OverlayService,
     PersianService,
-    UtilsService
+    UtilsService,
   ],
 })
 export class CoreModule {
+  constructor(private injector: Injector) {
+    NgGlobal.Injector = this.injector;
+  }
 }
