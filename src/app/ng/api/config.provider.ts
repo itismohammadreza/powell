7b7494443a8ab157c551d6999acc4e451f888ffc
Mohammad Reza <shahbaziweb@gmail.com>
2023-04-06 15:48:22 +0430

    provide primeng services in configProvider

--- src/app/ng/api/config.provider.ts
+++ src/app/ng/api/config.provider.ts
@@ -1,12 +1,17 @@
 import {DOCUMENT} from "@angular/common";
-import {PrimeNGConfig} from "primeng/api";
+import {ConfirmationService, FilterService, MessageService, PrimeNGConfig} from "primeng/api";
 import {NgConfig} from "@ng/models";
 import {ThemeService} from "@ng/api/theme.service";
 import {ConfigService} from "@ng/api/config.service";
+import {DialogService} from "primeng/dynamicdialog";
 
 export function initiateNgConfigProvider(ngConfig?: NgConfig) {
   return [
     ThemeService,
+    MessageService,
+    DialogService,
+    ConfirmationService,
+    FilterService,
     {
       provide: ConfigService,
       useFactory: (primengConfig: PrimeNGConfig, themeService: ThemeService, document: Document) => {
