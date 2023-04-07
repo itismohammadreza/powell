0e8e7c6c8e139b543c095a6cdb1d3b044c5b4333
Mohammad Reza <shahbaziweb@gmail.com>
2023-04-07 13:47:11 +0430

    add missing api models to primeng

--- src/app/ng/primeng/api/api.ts
+++ src/app/ng/primeng/api/api.ts
@@ -1,12 +1,34 @@
-import {ZIndexUtils} from "primeng/utils";
-import {FilterMetadata, LazyLoadEvent, MenuItem, OverlayOptions, SortMeta} from 'primeng/api';
+import {UniqueComponentId, ZIndexUtils} from "primeng/utils";
+import {
+  FilterMetadata,
+  LazyLoadEvent,
+  MenuItem,
+  OverlayOptions,
+  SortMeta,
+  ConfirmationService,
+  FilterService,
+  MessageService,
+  PrimeNGConfig,
+  Confirmation,
+  ConfirmEventType,
+  Message
+} from 'primeng/api';
 import {DomHandler} from 'primeng/dom';
+import {ScrollerOptions} from "primeng/scroller";
 
 export const PrimeZIndexUtils = ZIndexUtils;
 export const PrimeDomHandler = DomHandler;
+export const PrimeConfirmationService = ConfirmationService;
+export const PrimeFilterService = FilterService;
+export const PrimeMessageService = MessageService;
+export const PrimeConfig = PrimeNGConfig;
+export const PrimeConfirmEventType = ConfirmEventType;
 
+export type PrimeScrollerOptions = ScrollerOptions;
 export type PrimeOverlayOptions = OverlayOptions;
 export type PrimeFilterMetadata = FilterMetadata;
 export type PrimeSortMeta = SortMeta;
 export type PrimeLazyLoadEvent = LazyLoadEvent;
 export type PrimeMenuItem = MenuItem;
+export type PrimeConfirmation = Confirmation;
+export type PrimeMessage = Message;
