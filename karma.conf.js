1be088ede17b9edee6e889f60c84be095e132439
Mohammad Reza <shahbaziweb@gmail.com>
2023-04-07 16:11:44 +0430

    rename prime-crud to prime-plus

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/prime-crud'),
+      dir: require('path').join(__dirname, './coverage/prime-plus'),
       subdir: '.',
       reporters: [
         { type: 'html' },
