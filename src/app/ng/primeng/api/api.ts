d815c8166c02d664f3805cb428e79354ef6f9668
Mohammad Reza <shahbaziweb@gmail.com>
2023-04-07 13:21:22 +0430

    create primeng api directory

---
+++ src/app/ng/primeng/api/api.ts
@@ -0,0 +1,12 @@
+import {ZIndexUtils} from "primeng/utils";
+import {FilterMetadata, LazyLoadEvent, MenuItem, OverlayOptions, SortMeta} from 'primeng/api';
+import {DomHandler} from 'primeng/dom';
+
+export const PrimeZIndexUtils = ZIndexUtils;
+export const PrimeDomHandler = DomHandler;
+
+export type PrimeOverlayOptions = OverlayOptions;
+export type PrimeFilterMetadata = FilterMetadata;
+export type PrimeSortMeta = SortMeta;
+export type PrimeLazyLoadEvent = LazyLoadEvent;
+export type PrimeMenuItem = MenuItem;
