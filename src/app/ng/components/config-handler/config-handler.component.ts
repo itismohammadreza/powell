c593bb656a65d1e7c37ffc8ba6d97512a6f1542c
Mohammad Reza <shahbaziweb@gmail.com>
2023-04-23 19:51:44 +0430

    create config-handler.component

---
+++ src/app/ng/components/config-handler/config-handler.component.ts
@@ -0,0 +1,94 @@
+import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges} from '@angular/core';
+import {Subject, takeUntil} from "rxjs";
+import {NgConfig, NgFixLabelPosition, NgLabelPosition, NgSize} from "@ng/models";
+import {ConfigService} from "@ng/api";
+import {Global} from "@core/config";
+
+@Component({template: ''})
+export class ConfigHandlerComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() rtl: boolean;
+  @Output() rtlChange = new EventEmitter();
+  @Input() fixLabelPos: NgFixLabelPosition;
+  @Output() fixLabelPosChange = new EventEmitter();
+  @Input() labelPos: NgLabelPosition;
+  @Output() labelPosChange = new EventEmitter();
+  @Input() filled: boolean;
+  @Output() filledChange = new EventEmitter();
+  @Input() inputSize: NgSize;
+  @Output() inputSizeChange = new EventEmitter();
+  @Input() showRequiredStar: boolean;
+  @Output() showRequiredStarChange = new EventEmitter();
+  @Input() disableConfigChangeEffect: boolean;
+  @Output() disableConfigChangeEffectChange = new EventEmitter();
+
+  @Output() configChange = new EventEmitter();
+
+  stopConfigSubscription$: Subject<any>;
+  configService: ConfigService;
+
+  constructor() {
+    this.configService = Global.Injector.get(ConfigService);
+  }
+
+  ngOnInit() {
+    // add initial configService value to above Inputs and then emit them to apply to components.
+    this.applyConfig(this.configService.getConfig(), this.configService.getConfig())
+    if (this.disableConfigChangeEffect) {
+      return
+    }
+    this.startSubscription();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const {firstChange = true, previousValue, currentValue} = changes.disableConfigChangeEffect || {};
+    if (firstChange) {
+      return
+    }
+    if (currentValue === true) {
+      this.stopSubscription()
+    } else if (currentValue === false && previousValue === true) {
+      this.startSubscription()
+    }
+  }
+
+  ngOnDestroy() {
+    this.stopSubscription();
+  }
+
+  startSubscription() {
+    this.stopConfigSubscription$ = new Subject<boolean>();
+    this.configService.configChange$.pipe(takeUntil(this.stopConfigSubscription$))
+      .subscribe(({modifiedConfig, currentConfig}) => {
+        this.applyConfig(modifiedConfig, currentConfig);
+        this.configChange.emit({modifiedConfig, currentConfig});
+        if (currentConfig.disableConfigChangeEffect) {
+          this.stopSubscription();
+        }
+      })
+  }
+
+  stopSubscription() {
+    if (this.stopConfigSubscription$) {
+      this.stopConfigSubscription$.next(true);
+      this.stopConfigSubscription$.complete();
+    }
+  }
+
+  applyConfig(modifiedConfig: NgConfig, currentConfig: NgConfig) {
+    const configs: (keyof NgConfig)[] = [
+      'disableConfigChangeEffect',
+      'rtl',
+      'fixLabelPos',
+      'labelPos',
+      'filled',
+      'inputSize',
+      'showRequiredStar'
+    ]
+    configs.forEach(config => {
+      this[config] = currentConfig[config];
+      if (modifiedConfig[config] != undefined) {
+        this[`${config}Change`].emit(this[config]);
+      }
+    })
+  }
+}
