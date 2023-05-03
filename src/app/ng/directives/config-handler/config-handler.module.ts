7978964903120201dcf6c4131af8bc8f9f31286e
PC-22 <shahbaziweb@gmail.com>
2023-05-03 15:57:06 +0330

    remove config-handler directive

--- src/app/ng/directives/config-handler/config-handler.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core';
-import {ConfigHandlerDirective} from "@ng/directives/config-handler";
-
-@NgModule({
-  declarations: [ConfigHandlerDirective],
-  exports: [ConfigHandlerDirective]
-})
-export class ConfigHandlerModule {
-}
