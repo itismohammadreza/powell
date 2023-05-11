d71a7ff578878f93f276d262dc482afb76a3e1cd
Mohammad Reza <shahbaziweb@gmail.com>
2023-05-11 18:23:00 +0330

    handle bootstrap files in main component

--- src/app/modules/main/main.page.ts
+++ src/app/modules/main/main.page.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {Global} from "@core/config";
+import {ConfigService} from "@powell/api";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'ng-main-page',
@@ -8,4 +10,24 @@ import {Global} from "@core/config";
   animations: [Global.Config.routeAnimation]
 })
 export class MainPage {
+  constructor(private configService: ConfigService, @Inject(DOCUMENT) private document: Document) {
+    this.handleBootstrapFiles(Global.Config.rtl);
+    this.configService.configChange$.subscribe(res => {
+      this.handleBootstrapFiles(res.currentConfig.rtl)
+    })
+  }
+
+  private handleBootstrapFiles(rtl: boolean) {
+    let bootStrapLinkEl: any = this.document.querySelector('#bootstrap-style-link');
+    if (!bootStrapLinkEl) {
+      const head = this.document.querySelector('head');
+      bootStrapLinkEl = this.document.createElement('link');
+      bootStrapLinkEl.id = "bootstrap-style-link";
+      bootStrapLinkEl.rel = "stylesheet";
+      bootStrapLinkEl.type = "text/css";
+      head.appendChild(bootStrapLinkEl);
+    }
+    const themeLink = `assets/styles/vendor/bootstrap/bootstrap${rtl ? '.rtl' : ''}.css`;
+    bootStrapLinkEl.setAttribute('href', themeLink);
+  }
 }
