7edf84f1cefb0e0e19657bb18bcd56df78c0a596
Mohammad Reza <shahbaziweb@gmail.com>
2023-05-11 19:14:26 +0330

    move bootstrap handler to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {ConfigService} from "@powell/api";
+import {DOCUMENT} from "@angular/common";
+import {Global} from "@core/config";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,24 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private configService: ConfigService, @Inject(DOCUMENT) private document: Document) {
+    this.handleBootstrapFiles(Global.Config.rtl);
+    this.configService.configChange$.subscribe(res => {
+      this.handleBootstrapFiles(res.currentConfig.rtl)
+    })
+  }
+
+  private handleBootstrapFiles(rtl: boolean) {
+    let bootStrapLinkEl: any = this.document.querySelector('#bootstrap-style-link');
+    if (!bootStrapLinkEl) {
+      const head = this.document.querySelector('head');
+      bootStrapLinkEl = this.document.createElement('link');
+      bootStrapLinkEl.id = "bootstrap-style-link";
+      bootStrapLinkEl.rel = "stylesheet";
+      bootStrapLinkEl.type = "text/css";
+      head.appendChild(bootStrapLinkEl);
+    }
+    const themeLink = `assets/styles/vendor/bootstrap/bootstrap${rtl ? '.rtl' : ''}.css`;
+    bootStrapLinkEl.setAttribute('href', themeLink);
+  }
 }
--- src/app/modules/main/main.page.ts
+++ src/app/modules/main/main.page.ts
@@ -1,7 +1,5 @@
-import {Component, Inject} from '@angular/core';
+import {Component} from '@angular/core';
 import {Global} from "@core/config";
-import {ConfigService} from "@powell/api";
-import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'ng-main-page',
@@ -10,24 +8,4 @@ import {DOCUMENT} from "@angular/common";
   animations: [Global.Config.routeAnimation]
 })
 export class MainPage {
-  constructor(private configService: ConfigService, @Inject(DOCUMENT) private document: Document) {
-    this.handleBootstrapFiles(Global.Config.rtl);
-    this.configService.configChange$.subscribe(res => {
-      this.handleBootstrapFiles(res.currentConfig.rtl)
-    })
-  }
-
-  private handleBootstrapFiles(rtl: boolean) {
-    let bootStrapLinkEl: any = this.document.querySelector('#bootstrap-style-link');
-    if (!bootStrapLinkEl) {
-      const head = this.document.querySelector('head');
-      bootStrapLinkEl = this.document.createElement('link');
-      bootStrapLinkEl.id = "bootstrap-style-link";
-      bootStrapLinkEl.rel = "stylesheet";
-      bootStrapLinkEl.type = "text/css";
-      head.appendChild(bootStrapLinkEl);
-    }
-    const themeLink = `assets/styles/vendor/bootstrap/bootstrap${rtl ? '.rtl' : ''}.css`;
-    bootStrapLinkEl.setAttribute('href', themeLink);
-  }
 }
