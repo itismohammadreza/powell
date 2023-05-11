72971fa1c94baab58e24413a421d0858a9918982
Mohammad Reza <shahbaziweb@gmail.com>
2023-05-11 13:27:10 +0330

    remove class base guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,18 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {inject, NgModule} from '@angular/core';
+import {Router, RouterModule, Routes, UrlSegment} from '@angular/router';
 import {NotFoundPage} from "@modules/layout/not-found/not-found.page";
+import {AuthService} from "@core/http";
+
+const canActivate = (next: any) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (authService.hasToken()) {
+    return true;
+  } else {
+    router.navigate(['/auth/login'], {queryParams: {returnUrl: next.path}});
+    return false;
+  }
+}
 
 export const routes: Routes = [
   {
@@ -11,7 +23,7 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('@modules/main/main.module').then((m) => m.MainModule),
-    // canMatch: [AuthGuard],
+    // canMatch: [canActivate],
   },
   {
     path: '404',
