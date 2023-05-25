c35f07e86aaa902be566e57e101c4a46d1517304
mohammadreza99 <shahbaziweb@gmail.com>
2023-05-25 15:52:40 +0330

    set default display of all components to block

--- src/app/powell/components/button/button.module.ts
+++ src/app/powell/components/button/button.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {ButtonComponent} from "@powell/components/button";
 import {PrimeButtonModule} from "@powell/primeng";
+import {TemplateModule} from "@powell/directives/template";
 
 @NgModule({
   declarations: [ButtonComponent],
   exports: [ButtonComponent],
-  imports: [PrimeButtonModule, CommonModule],
+  imports: [PrimeButtonModule, CommonModule, TemplateModule],
 })
 export class ButtonModule {
 }
