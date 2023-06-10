560c3f92b8be9ea5578d215b4f9c16419ad37b02
mohammadreza9 <shahbaziweb@gmail.com>
2023-06-10 12:23:45 +0330

    minor updates

--- src/app/powell/api/utils.service.ts
+++ src/app/powell/api/utils.service.ts
@@ -154,8 +154,8 @@ export class UtilsService {
     return result;
   }
 
-  isValidURL(string: string) {
-    const res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  isValidURL(url: string) {
+    const res = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     return (res !== null)
   };
 
