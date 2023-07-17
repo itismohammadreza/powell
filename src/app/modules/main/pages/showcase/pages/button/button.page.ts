76c79e8ec4dfca126d741faf39e7dec153473f33
mohammadreza9 <shahbaziweb@gmail.com>
2023-07-17 12:11:49 +0330

    add async function callback to button demo

--- src/app/modules/main/pages/showcase/pages/button/button.page.ts
+++ src/app/modules/main/pages/showcase/pages/button/button.page.ts
@@ -19,13 +19,13 @@ export class ButtonPage {
   color: NgColor = 'primary';
   badgeColor: NgColor = 'secondary';
   size: NgSize = 'md';
-  async: boolean = true;
+  async: boolean = false;
   newLabel: string = 'New Label';
   newColor: NgColor = 'secondary';
 
   onClickAsync(loadingCallback) {
     setTimeout(() => {
-      loadingCallback()
+      loadingCallback(true)
     }, 3000)
   }
 }
