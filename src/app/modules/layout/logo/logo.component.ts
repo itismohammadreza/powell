d41cd81661c9a997c6c012321232a7eedc944741
mohammadreza9 <shahbaziweb@gmail.com>
2023-07-22 14:13:36 +0330

    implement logo

--- src/app/modules/layout/logo/logo.component.ts
+++ src/app/modules/layout/logo/logo.component.ts
@@ -1,15 +1,18 @@
 import {Component, Input} from '@angular/core';
 import {RouterModule} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'ng-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss'],
   standalone: true,
-  imports: [RouterModule]
+  imports: [RouterModule, CommonModule]
 })
 export class LogoComponent {
   @Input() width: string = '100px';
+  @Input() type: 'light-vertical' | 'light-horizontal' | 'dark-vertical' | 'dark-horizontal' | 'logo-only' = 'dark-horizontal';
+  @Input() src: string = null; /* /assets/images/logo.png */
   @Input() link: string[] = ['/'];
   @Input() disableNavigation: boolean = false;
 }
