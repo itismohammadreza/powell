30ca7d09c4d87998b26f5c948059a31e46fa0a1c
mohammadreza99 <shahbaziweb@gmail.com>
2024-08-22 14:38:34 +0330

    add tailwind (instead bootstrap) and remove primeicons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {ConfigService} from "@powell/api";
-import {DOCUMENT} from "@angular/common";
-import {globalConfig} from "@core/config";
 
 @Component({
   selector: 'app-root',
@@ -11,29 +8,5 @@ import {globalConfig} from "@core/config";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  private document = inject(DOCUMENT);
-  private configService = inject(ConfigService);
-
-  ngOnInit() {
-    this.handleBootstrapFiles(globalConfig.rtl);
-    this.configService.configChange$.subscribe(res => {
-      this.handleBootstrapFiles(res.currentConfig.rtl)
-    })
-  }
-
-  handleBootstrapFiles(rtl: boolean) {
-    let bootstrapLink: HTMLLinkElement = this.document.querySelector('#bootstrap-style-link');
-    const {documentElement, head} = this.document;
-    const themeLink = `assets/styles/vendor/bootstrap/bootstrap${rtl ? '.rtl' : ''}.css`;
-    if (!bootstrapLink) {
-      bootstrapLink = this.document.createElement('link');
-      bootstrapLink.id = "bootstrap-style-link";
-      bootstrapLink.rel = "stylesheet";
-      bootstrapLink.type = "text/css";
-      head.insertBefore(bootstrapLink, head.childNodes[1]);
-    }
-    bootstrapLink.setAttribute('href', themeLink);
-    documentElement.setAttribute('dir', rtl ? 'rtl' : 'ltr');
-  }
+export class AppComponent {
 }
