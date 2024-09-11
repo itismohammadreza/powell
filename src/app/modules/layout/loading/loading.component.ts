9bb10a0419e48b9f57e1d2ffa0c17addc307fd70
Your Name <you@example.com>
2024-09-11 14:54:02 +0330

    refactor loading component with tailwind

--- src/app/modules/layout/loading/loading.component.ts
+++ src/app/modules/layout/loading/loading.component.ts
@@ -1,11 +1,15 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {LoaderService} from '@core/utils';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'ng-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
   standalone: true,
+  imports: [
+    NgClass
+  ]
 })
 export class LoadingComponent implements OnInit {
   private loaderService = inject(LoaderService);
