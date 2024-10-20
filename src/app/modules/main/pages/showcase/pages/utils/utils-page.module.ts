5b73532f5aace6941756cb5cd5ea9129c9a8dc28
Your Name <you@example.com>
2024-10-20 16:17:59 +0330

    remove dialog2 component

--- src/app/modules/main/pages/showcase/pages/utils/utils-page.module.ts
+++ src/app/modules/main/pages/showcase/pages/utils/utils-page.module.ts
@@ -3,14 +3,12 @@ import {ExtrasModule} from "@modules/main/pages/showcase/extras.module";
 import {RouterModule} from "@angular/router";
 import {DynamicDialogSampleComponent, UtilsPage} from "@modules/main/pages/showcase/pages/utils";
 import {ButtonModule} from "@powell/components/button";
-import {OverlayModule} from "@powell/components/overlay";
 import {InputNumberModule} from "@powell/components/input-number";
 
 @NgModule({
   declarations: [UtilsPage, DynamicDialogSampleComponent],
   imports: [
     ButtonModule,
-    OverlayModule,
     InputNumberModule,
     ExtrasModule,
     RouterModule.forChild([{path: '', component: UtilsPage}])
