a2ae4e8eaad83edcbf9ed4990c63c26b13210317
itismohammadreza <shahbaziweb@gmail.com>
2025-05-23 13:41:36 +0000

    Deploying to gh-pages from @ itismohammadreza/powell@8b47f02e89486934f3ddb772468d1f799ca8516f 🚀

---
+++ browser/chunk-U2DC4TTA.js
@@ -0,0 +1,1 @@
+import{b as i}from"./chunk-3I7JFZ4A.js";import"./chunk-ZPS7A6G6.js";import"./chunk-KVRY6YNR.js";import"./chunk-VV4CBK2G.js";import{ca as t,kb as e}from"./chunk-DQ4BICSX.js";var s=(()=>{class o{static \u0275fac=function(r){return new(r||o)};static \u0275mod=e({type:o});static \u0275inj=t({imports:[i]})}return o})();export{s as ShowcaseModule};
