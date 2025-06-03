22a1e4bbcdd20698c2277e2dd82a4f9184f6bbc1
Your Name <you@example.com>
2025-06-03 11:54:06 +0330

    remove date pipe

--- src/app/powell/pipes/date/date.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core';
-import {DatePipe} from "@powell/pipes/date";
-
-@NgModule({
-  declarations: [DatePipe],
-  exports: [DatePipe]
-})
-export class DateModule {
-}
