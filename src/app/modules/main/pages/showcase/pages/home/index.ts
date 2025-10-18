7ec2c48983f123e1ea666c3713e655c179721941
Your Name <you@example.com>
2025-10-18 14:11:43 +0330

    remove funny home page components

--- src/app/modules/main/pages/showcase/pages/home/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './blocks/blocks.component';
-export * from './bubbles/bubbles.component';
-export * from './emoji/emoji.component';
-export * from './smoke/smoke.component';
-export * from './space/space.component';
-export * from './welcome/welcome.component';
-export * from './home.page';
--- src/app/modules/main/pages/showcase/pages/home/welcome/welcome.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
-})
-export class WelcomeComponent {
-}
